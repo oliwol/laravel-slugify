850709376d4cb010616534959268e1f0eae3f8a4
Oliver Wolschke <1193898+oliwol@users.noreply.github.com>
2026-01-11 09:51:03 +0100

    chore: add return type

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -85,7 +85,7 @@ trait HasSlug
         return is_string($value);
     }
 
-    public function scopeSlugQuery($query)
+    public function scopeSlugQuery($query): Builder
     {
         return $query;
     }
